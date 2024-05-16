1446a883284657366510a125c859c096aef80b0d
fckoo <36943117+fckoo@users.noreply.github.com>
2024-05-16 21:22:54 +0000

    added files

---
+++ SharpLigoloNG/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace SharpLigoloNG
+{
+    class Program
+    {
+        [DllImport("main.dll", EntryPoint = "mainwrapper")]
+        extern static int mainwrapper(byte[] args);
+
+        static void Main(string[] args)
+        {
+            string argsAsString = String.Join(" ", args);
+            mainwrapper(Encoding.Default.GetBytes(argsAsString));
+        }
+    }
+}
